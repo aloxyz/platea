770f9317100f4139b3f12a57b6bf35c3c8ecf3f4
aloxyz <aloisiogabriele@gmail.com>
2022-09-17 23:05:21 +0200

    wrote request methods

--- platea/src/main/java/alo/Client.java
+++ platea/src/main/java/alo/Client.java
@@ -1,22 +1,29 @@
 package alo;
 
-import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.core.DefaultDockerClientConfig;
-import com.github.dockerjava.core.DockerClientConfig;
-import com.github.dockerjava.core.DockerClientImpl;
-import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient;
-import com.github.dockerjava.transport.DockerHttpClient.Request;
-import com.github.dockerjava.transport.DockerHttpClient.Response;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpResponse.BodyHandler;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.http.client.utils.URIBuilder;
 
 
 public class Client {
+    private final BodyPublisher noBody = HttpRequest.BodyPublishers.noBody();
+    private HttpClient httpClient;
+    
     private static Client client;
-    private DockerClient dockerClient;
+    
+    //private DockerClient dockerClient;
 
     private Client() throws Exception {
-        connect();
+        httpClient = HttpClient.newHttpClient();
     } 
  
     public static synchronized Client getClient() throws Exception {
@@ -26,37 +33,73 @@ public class Client {
         return client;
     }
 
-    public DockerClient getDockerClient() {
-        return dockerClient;
+    public URI uriBuilder(String path, Map<String, String> parameters) throws Exception{
+        URIBuilder builder = new URIBuilder();
+        builder.setScheme("http").setHost(Config.getConfig().dockerSocket()).setPath(path);
+
+        
+
+        for (Map.Entry<String, String> pair : parameters.entrySet()) {
+            builder.setParameter(pair.getKey(), pair.getValue());
+        }
+
+        return builder.build();
     }
 
+    public HttpRequest get(String path) throws Exception {
+        URI uri = uriBuilder(path, Collections.<String, String>emptyMap());
 
-    public int connect() throws Exception {
-        org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
-            // Docker client configuration
-            DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-            .withDockerHost("unix:///var/run/docker.sock").build();
-            
-            // Establishing a connection with the Docker socket
-            DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
-            .dockerHost(config.getDockerHost())
-            .sslConfig(config.getSSLConfig())
-            .maxConnections(100)
-            .connectionTimeout(Duration.ofSeconds(30))
-            .responseTimeout(Duration.ofSeconds(45))
+        return
+        HttpRequest.newBuilder(uri)
+            .timeout(Duration.ofSeconds(10))
+            .GET()
             .build();
-    
-            Request request = Request.builder()
-            .method(Request.Method.GET)
-            .path("/_ping")
+    }
+
+    public HttpRequest post(String path, BodyPublisher body) throws Exception {
+        URI uri = uriBuilder(path, Collections.<String, String>emptyMap());
+
+        return
+        HttpRequest.newBuilder(uri)
+            .timeout(Duration.ofSeconds(10))
+            .POST(body)
             .build();
-    
-            // Check response and do the rest
-            Response response = httpClient.execute(request);
-            DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
-            if (response.getStatusCode() == 200) {
-                this.dockerClient = dockerClient;
-            }
-            return response.getStatusCode();
-        }
+    }
+
+    public HttpResponse sendRequest(HttpRequest method, BodyHandler bHandler) throws Exception {
+        return
+        this.httpClient
+        .send(method, bHandler);
+    }
+
+    public HttpResponse getResource(String path, Map<String, String> parameters) throws Exception{
+        return
+        sendRequest(
+            get(uriBuilder(path, parameters).toString()), 
+            BodyHandlers.ofString());
+    }
+
 }
+    
+
+
+
+
+/*
+ * HttpRequest.BodyPublishers.ofString("Sample request body")
+ * HttpRequest.BodyPublishers.ofByteArray(sampleData)
+ * HttpRequest.BodyPublishers.fromFile(Paths.get("src/test/resources/sample.txt"))
+ * 
+ * BodyHandlers.ofByteArray
+ * BodyHandlers.ofString
+ * BodyHandlers.ofFile
+ * BodyHandlers.discarding
+ * BodyHandlers.replacing
+ * BodyHandlers.ofLines
+ * BodyHandlers.fromLineSubscriber
+ * 
+ * HttpResponse.statusCode(), HttpURLConnection 
+ * HttpResponse.body(), return type depends on the response BodyHandler parameter passed to the send() method
+ * HttpResponse.headers()
+ */
+    
